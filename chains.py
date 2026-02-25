8bee7c91c1bbb0bedfd6d88d69457923b78270e2
Upnit-b <upnit.singh@gmail.com>
2026-02-25 07:52:12 -0500

    adding project file

---
+++ chains.py
@@ -0,0 +1,33 @@
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+generation_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system", "You are a twitter techie influencer assistant tasked with writing excellent twitter posts."
+            "Generate the best twitter post possible for the user's request."
+            "If the user provides critique, respond with a revised version of your previous attempts."
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+reflection_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a viral twitter influencer grading a tweet. Generate critique and recommendations for the user's tweet."
+            "Always provide detailed recommendations, including requests for length, virality, style, etc."
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+
+llm = ChatGroq(model="meta-llama/llama-4-maverick-17b-128e-instruct")
+
+generation_chain = generation_prompt | llm
+reflection_chain = reflection_prompt | llm
--- main.py
+++ main.py
@@ -1,5 +1,66 @@
+from typing import List, Sequence, TypedDict
+from dotenv import load_dotenv
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.graph import START, END, StateGraph, MessagesState
+from chains import generation_chain, reflection_chain
+
+load_dotenv()
+
+REFLECT = "reflect"
+GENERATE = "generate"
+
+
+def generate_node(state: MessagesState) -> MessagesState:
+    response = generation_chain.invoke({
+        "messages": state["messages"]
+    })
+    return {"messages": response}
+
+
+def reflection_node(state: MessagesState) -> MessagesState:
+    response = reflection_chain.invoke({
+        "messages": state["messages"]
+    })
+    return {"messages": HumanMessage(content=response.content)}
+
+
 def main():
-    print("Hello from basic-reflection-agent!")
+
+    graph = StateGraph(MessagesState)
+    graph.add_node(GENERATE, generate_node)
+    graph.add_node(REFLECT, reflection_node)
+
+    graph.set_entry_point(GENERATE)
+
+    graph.add_edge(START, GENERATE)
+
+    def should_continue(state: MessagesState):
+        if (len(state["messages"]) > 4):
+            return END
+        return REFLECT
+
+    graph.add_conditional_edges(GENERATE, should_continue, {
+                                REFLECT: REFLECT, END: END})
+    graph.add_edge(REFLECT, GENERATE)
+
+    app = graph.compile()
+
+    response = app.invoke({"messages": HumanMessage(
+        content="AI Agents taking over the world")})
+
+    print("\n\n")
+    print(response)
+
+    # inputs = {"messages": HumanMessage(content="AI Agents taking over content creation")}
+
+    # for output in app.stream(inputs):
+    #     for key, value in output.items():
+    #         print(f"Output from node {key}: ")
+    #         print(value)
+    #         print("-------------------------------")
+
+    # print(app.get_graph().draw_mermaid())
+    # app.get_graph().print_ascii()
 
 
 if __name__ == "__main__":
